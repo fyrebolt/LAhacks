e757ee1c49b320cdb29e43880228e55d14760e92
Neel32 <77811207+midnight0s@users.noreply.github.com>
2024-04-06 13:40:59 -0700

    comment out authchanged

--- firebase.js
+++ firebase.js
@@ -51,14 +51,14 @@ function signIn(email, password) {
     });
 }
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    const uid = user.uid;
-    // ...
-  } else {
-    // User is signed out
-    // ...
-  }
-});
+// onAuthStateChanged(auth, (user) => {
+//   if (user) {
+//     // User is signed in, see docs for a list of available properties
+//     // https://firebase.google.com/docs/reference/js/auth.user
+//     const uid = user.uid;
+//     // ...
+//   } else {
+//     // User is signed out
+//     // ...
+//   }
+// });
