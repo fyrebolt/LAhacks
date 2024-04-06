0e672e97afb72e972c4aff863e9e07b93fc1b212
midnight0s <77811207+midnight0s@users.noreply.github.com>
2024-04-06 12:26:25 -0700

    Move stuff out of site folder

--- firebase.js
+++ firebase.js
@@ -1,4 +1,21 @@
-firebase.initializeApp(firebaseConfig);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtEWmEswnxOur3OIBAWX1oZ82aPY9-yfE",
+  authDomain: "lahacks8.firebaseapp.com",
+  databaseURL: "https://lahacks8-default-rtdb.firebaseio.com",
+  projectId: "lahacks8",
+  storageBucket: "lahacks8.appspot.com",
+  messagingSenderId: "1054684978705",
+  appId: "1:1054684978705:web:0403789073974326321bb0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const database = firebase.database();
 
   
