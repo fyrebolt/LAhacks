0116267623885a257555c0bd116c8fbdaa7204c4
Neel32 <77811207+midnight0s@users.noreply.github.com>
2024-04-06 13:21:13 -0700

    fix imports

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
