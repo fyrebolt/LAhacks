3e1ea18672166ae18d517351a8abd788035fa5ae
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2024-04-06 16:41:33 -0700

    Delete api directory

--- api/main.js
+++
@@ -1,9 +0,0 @@
-const { dockStart } = require('node-nlp');
-
-(async () => {
-  const dock = await dockStart();
-  const nlp = dock.get('nlp');
-  await nlp.train();
-  const response = await nlp.process('en', 'Who are you');
-  console.log(response);
-})();
