20f4d79f17041a026ce4fd2edd18dff08c936cf7
Neel32 <77811207+midnight0s@users.noreply.github.com>
2024-04-07 00:25:15 -0700

    Added typewriter effect

--- index.js
+++ index.js
@@ -7,21 +7,66 @@ function load(){
     wrapper.style.transition = "0.75 ease"
 }
    
+// TODO implement auth if we have extra time
 
-    
+document.addEventListener('DOMContentLoaded', function (event) {
+    // array with texts to type in typewriter
+    var dataText = ['NotDeafLink', 'NotDeafLink', 'NotDeafLink'];
 
+    // type one text in the typwriter
+    // keeps calling itself until the text is finished
+    function typeWriter(text, i, fnCallback) {
+        // chekc if text isn't finished yet
+        if (i < (text.length)) {
+            // add next character to header
+            document.getElementById('appName').innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
 
+            // wait for a while and call this function again for next character
+            setTimeout(function () {
+                typeWriter(text, i + 1, fnCallback)
+            }, 150);
+        }
+        // text finished, call callback if there is a callback function
+        else if (typeof fnCallback == 'function') {
+            // call callback after timeout
+            setTimeout(fnCallback, 700);
+        }
+    }
+    // start a typewriter animation for a text in the dataText array
+    function StartTextAnimation(i) {
+        if (typeof dataText[i] == 'undefined') {
+            setTimeout(function () {
+                StartTextAnimation(0);
+            }, 20000);
+        }
+        // check if dataText[i] exists
+        if (i < dataText[i].length) {
+            // text exists! start typewriter animation
+            if (i == 1) {
+                document.getElementById("appName").style.fontFamily = 'ASL'
+            } else {
+                document.getElementById("appName").style.fontFamily = 'Red Hat Display'
+            }
+            typeWriter(dataText[i], 0, function () {
+                // after callback (and whole text has been animated), start next text
+                StartTextAnimation(i + 1);
+            });
+        }
+    }
+    // start the text animation
+    StartTextAnimation(0);
+});
 
 button.onclick = async (event) => {
     event.preventDefault()
     wrapper.style.transform = "translateY(100vh)";
-    var msg = new SpeechSynthesisUtterance();
-    msg.text = "i love men";
+    // var msg = new SpeechSynthesisUtterance();
+    // msg.text = "i love men";
     window.speechSynthesis.speak(msg);
     setTimeout(function(){
         wrapper.style.display = "none";
         page2.style.transform = "translateY(0vh)"; 
+        window.location = 'vision.html'
     }, 750)
-    
-    
 }
+
