366514e3ffe93c115447d7dcfe9f987257a4e873
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2024-04-06 22:16:41 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const button = document.getElementById("button")
 const wrapper = document.getElementById("wrapper")
-
+const page2 = document.getElementById("page2")
 function load(){
-    wrapper.style.transform = "translateY(0px)";
+    wrapper.style.transform = "translateY(-25vh)";
     wrapper.style.opacity = "1"
     wrapper.style.transition = "0.75 ease"
 }
+   
 
     
 
@@ -13,12 +14,14 @@ function load(){
 
 button.onclick = async (event) => {
     event.preventDefault()
+    wrapper.style.transform = "translateY(100vh)";
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = "i love men";
+    window.speechSynthesis.speak(msg);
     setTimeout(function(){
-        wrapper.style.display = "none"
-        page2.style.display = "flex"
+        wrapper.style.display = "none";
+        page2.style.transform = "translateY(0vh)"; 
     }, 750)
-    wrapper.style.transform = "translateY(700px)";
-        
     
     
 }
